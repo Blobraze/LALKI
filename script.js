c25c4da667221bc1ae3c424e1e6266336bc3d70e
Blobraze <virusmati123@gmail.com>
2023-12-17 19:15:34 +0100

    footer

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function showSidebar() {
-    document.getElementById("sideNavbar").style.display = "block";
+    //document.getElementById("sideNavbar").style.display = "block";
+    document.getElementById("sideNavbar").style.left = "0px";
+    //document.getElementsByClassName("sideNavbar") += " active";
 }
 
 function hideSidebar() {
-    document.getElementById("sideNavbar").style.display = "none";
+    document.getElementById("sideNavbar").style.left = "-220px";
 }
